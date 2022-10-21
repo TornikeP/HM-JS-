b0708a8f0f0b0ff3184f64ffd1647f07b690d048
TornikeP <112972102+TornikeP@users.noreply.github.com>
2022-10-21 20:54:34 +0400

    ძლივს

---
+++ script.js
@@ -0,0 +1,41 @@
+alert("იმედია მოვასწრებ")
+
+var strSum = prompt("ბლა ბლა ბლა ...")
+
+
+var income = 5000;
+if (income < 5000) {
+    alert("დაბალი ხელფასი")
+}
+else if (income >= 5000 && income <= 5000) {
+    alert("საშუალო ხელფასი")
+}
+else {
+    alert("მაღალი ხელფასი")
+}
+
+
+
+var income = prompt("თქვენი ხელფასი?");
+
+if (income < 5000) {
+    alert("დაბალი ხელფასი");
+}
+
+else if (income == 5000) {
+    alert("საშუალო ხელფასი");
+}
+
+else {
+    alert("მაღალი ხელფასი");
+}
+
+
+
+
+
+
+
+
+
+
